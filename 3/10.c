405262e4db2854e9c9b6355b2148fd423403fcc8
withmy27 <140888383+withmy27@users.noreply.github.com>
2024-12-09 14:54:42 +0900

    Create 10.c

---
+++ 3/10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n = 0;
+  
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - (i + 1); j++) {
+            putchar(' ');
+        }
+        for (int j = 0; j < i + 1; j++) {
+            putchar('*');
+        }
+        putchar('\n');
+    }
+    
+    return 0;
+}

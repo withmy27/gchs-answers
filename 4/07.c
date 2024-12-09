0eec577a910b470cda24f89fab636c91e879726b
withmy27 <140888383+withmy27@users.noreply.github.com>
2024-12-09 18:27:31 +0900

    Update 07.c

--- 4/07.c
+++ 4/07.c
@@ -2,11 +2,11 @@
 
 int main() {
     int i, input;
-    char nums[30] = { 0, };
+    char nums[30] = { 0, }; // 30개의 숫자를 저장할 배열을 0으로 초기화
 
     for (i = 0; i < 28; i++) {
         scanf("%d", &input);
-        input = 1;
+        nums[input - 1] = 1;
     }
 
     for (i = 0; i < 30; i++) {

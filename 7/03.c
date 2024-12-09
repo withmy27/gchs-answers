201caab723609af80cd1be627dc170165b359633
withmy27 <140888383+withmy27@users.noreply.github.com>
2024-12-10 00:20:29 +0900

    Update 03.c

--- 7/03.c
+++ 7/03.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    char words[5][15];
+    char words[5][15] = { 0, };
     
-    for (int i = 0; i < 5; i += 1) {
+    for (int i = 0; i < 5; i++) {
         scanf("%s", words[i]);
     }
     
-    for (int j = 0; j < 15; j += 1) {
-        for (int i = 0; i < 5; i += 1) {
+    for (int j = 0; j < 15; j++) {
+        for (int i = 0; i < 5; i++) {
             if (words[i][j]) {
                 printf("%c", words[i][j]);
             }

3e252cdd2b112f85931c6f50d3ccd50fff5e0da0
withmy27 <140888383+withmy27@users.noreply.github.com>
2024-12-09 13:36:48 +0900

    Create 3.c

---
+++ 2/3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+  
+    scanf("%d", &a);
+
+    if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0) 
+      puts("1");
+    else 
+      puts("0");
+  
+    return 0;
+}

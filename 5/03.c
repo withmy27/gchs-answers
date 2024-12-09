4f3287784fd9700257ed7657bd7720d34c393001
withmy27 <140888383+withmy27@users.noreply.github.com>
2024-12-09 18:43:22 +0900

    Create 03.c

---
+++ 5/03.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    int len, t;
+
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++) {
+        scanf("%s", str);
+
+        for (int j = 0, len = 0; str[j] != '\0'; j++, len++);
+
+        printf("%c%c\n", str[0], str[len - 1]);
+    }
+
+    return 0;
+}
